e711663c5c7b75afafcb383be78b4dab9dd81f20
adlandh <adlandh@gmail.com>
2016-12-21 14:35:50 +0300

    Added some methods;

--- local/lib/cacheenginememcached.php
+++ local/lib/cacheenginememcached.php
@@ -25,7 +25,20 @@ class CacheEngineMemcached implements ICacheEngine
     /*
      * @var sid - for using with several websites
      */
-    private $sid;
+    private $sid = "BX";
+    /*
+     * @var read - bytes read
+     */
+
+    private $read = false;
+
+    /*
+     * @var written - bytes written
+    */
+
+    private $written = false;
+
+
 
 
     /*
@@ -67,6 +80,24 @@ class CacheEngineMemcached implements ICacheEngine
     {
         if(self::$obMemcached!=null)
             self::$obMemcached=null;
+    }
+
+    /*
+     * Returns number of bytes read from memcached or false if there were no read operation
+     */
+
+    public function getReadBytes()
+    {
+        return $this->read;
+    }
+
+    /*
+     * Returns number of bytes written to memcached or false if there were no write operation
+     */
+
+    public function getWrittenBytes()
+    {
+        return $this->written;
     }
 
 
